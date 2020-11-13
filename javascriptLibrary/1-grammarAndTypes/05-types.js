73524b3a4502cde4d6b1ea36c9a6ed65a86d3b81
Adi Gal <Adi@Adis-MacBook-Pro.local>
2020-11-12 21:32:30 -0500

    initial push

---
+++ javascriptLibrary/1-grammarAndTypes/05-types.js
@@ -0,0 +1,109 @@
+//Boolean
+var on = true;
+console.log(on);
+
+let off = false;
+console.log(off)
+
+//Boolean can represent: true/false, yes/no, on/off, 0/1
+
+//undefined 
+/*
+Undefined usually means a variable has been declared, but it's not been assigned a value
+*/
+
+let grass;
+console.log(grass);
+
+var undef = undefined;
+console.log(undef);
+
+//Null
+/*
+Null means a variable has been declared and assigned a value of null
+*/
+
+var empty = null;
+console.log(empty);
+
+/* Null and undefiend both represent variables with no value inside. Think of it this way -- null values are for gag gifts that are boxex intentionally given with nothing inside (because that's the gag).
+Undefined values are gifts given when athe person giving the gift has forgotten to put the gift inside (oops!).
+*/
+
+//Numbers
+let myLitteralAge = 90;
+console.log(myLitteralAge);
+
+let precise = 99999999999;
+console.log(precise);
+
+let rounded = 9999999999999999;
+console.log(rounded);
+
+let notQuite = 0.2 + 0.1;
+console.log(notQuite);
+
+let numberAreHard = (0.2 * 10 + 0.1 *10) / 10;
+console.log(numberAreHard);
+
+//Strings
+//String is any value within quotes; JS spits out the value within the quotes
+let stringOne ="double quotes";
+let stringTwo ='single quotes';
+console.log(stringOne,stringTwo)
+
+//string concatenation vs addition
+let first = 1050 + 100;
+let second = '1050' + '100';
+
+console.log(first);
+console.log(second);
+
+let third = 1050 + '100';
+console.log(third);
+console.log(typeof third);
+
+//Interpolation
+//lets you use strings with dynamic content (they require backtricks)
+
+//Example 1:
+let age = 32;
+let string = `my age is: ${age}`
+console.log(string)
+
+//Object
+/*
+What is an object?
+
+An object is a container that stores property names and their values
+(it can hold multiple data types)
+
+Deonted by {}
+Have keys and values--color (key): 'blue' (value), separated with a colon, each key-value pair is separated with a coma
+*/
+
+let hulk = {
+    color: 'green',
+    age: 42,
+    isStrong: true
+}
+console.log(hulk);
+console.log(hulk.age);
+
+
+//Arrays
+/*
+Arrays store multiple values in an ordered way. They, too, hold multiple data types. 
+
+Denoted by []
+Has values ('blue', 'green', 'yellow'), separated by commas
+*/
+
+var StepToBrushTeeth = ['uncap toothpaste', 'squeeze toothpaste', 'rinse brush', 'brush','clean toothbrush']
+//array indexing           0                  1                      2             3        4   
+//console.log(StepToBrushTeeth)
+console.log(StepToBrushTeeth[0]);
+console.log(typeof StepToBrushTeeth);
+
+
+
